199e1c1641db5b4e2d3d4b5e563aebab9b2a804d
MLNdLTurboPiyush <34052183+piy97467@users.noreply.github.com>
2021-08-01 11:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def predict():
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
-#if __name__ == "__main__":
-    #app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
